65028a1a8bbe098b737e92956abfaf90cae5d776
Leora <leorab@suite-sol.com>
2019-03-28 09:57:02 +0200

    add line boundary to plot

--- 5-logistic-regression.py
+++ 5-logistic-regression.py
@@ -54,34 +54,42 @@ def calc_gradient(X,Y,weights,learning_rate, max_iterations,precision):
     print("Found acceptable loss at iteration",i+1,loss[i])
     return theta
 
-def logistic_regression(arr1,arr2):
+def logistic_regression(arr1,arr2,addSquareFeatures):
     numPoints = 200
     boundary = .5
     learning_rate = .1
-    max_iterations = 500
+    max_iterations = 100
     bias = np.ones(numPoints * 2)
-    precision = .2
-    
-    plt.scatter(arr1[:, 0], arr1[:, 1], s=20, label='Category 1')
-    plt.scatter(arr2[:, 0], arr2[:, 1], s=20, label='Category 2')
-    plt.legend()
-    plt.show()   
+    precision = .1 
     
     X = np.column_stack((np.concatenate((arr1,arr2), axis=0),bias))
     Y = np.asarray(np.concatenate((np.ones(numPoints),np.zeros(numPoints)), axis=0))
-    
-    #Add x-coordinate squared and y-coodinate squared as features to boost results
-    x_squared = X[:,0] ** 2
-    y_squared = X[:,1] ** 2
-    
-    X = np.column_stack((X,x_squared,y_squared))
-    
-    orig_weights = np.ones(5).reshape(5,1)
+
+    if addSquareFeatures:
+        #Add x-coordinate squared and y-coodinate squared as features to boost results
+        x_squared = X[:,0] ** 2
+        y_squared = X[:,1] ** 2
+        X = np.column_stack((X,x_squared,y_squared))
+        orig_weights = np.ones(5).reshape(5,1)
+    else:
+        orig_weights = np.ones(3).reshape(3,1)
         
     X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=42)  
 
     train_weights = calc_gradient(X_train,Y_train,orig_weights,learning_rate,max_iterations,precision)
 
+    plt.scatter(arr1[:, 0], arr1[:, 1], s=15, label='Category 1')
+    plt.scatter(arr2[:, 0], arr2[:, 1], s=15, label='Category 2')
+    ax = plt.gca()
+    ax.autoscale(False)
+    x_vals = np.array(ax.get_xlim())
+    y_vals = -(x_vals * train_weights[0] + (train_weights[2]*1.2))/train_weights[1]
+    
+    plt.plot(x_vals, y_vals, '--', c="green")
+    
+    plt.legend()
+    plt.show()  
+    
     train_precision = np.mean(category_prediction(boundary,X_train,train_weights)==Y_train)
     test_precision = np.mean(category_prediction(boundary,X_test,train_weights)==Y_test)
     print('Train precision: {} \nTest precision: {}'.format(train_precision, test_precision)) 
@@ -89,8 +97,8 @@ def logistic_regression(arr1,arr2):
 
 arr1 = np.array(create_random_points([0,0],4.5,numPoints))
 arr2 = np.array(create_random_points([3,3],4.5,numPoints))
-logistic_regression(arr1,arr2)
+logistic_regression(arr1,arr2,False)
 
 circle1 = np.array(create_random_circular_points([0,0],1,numPoints))
 circle2 = np.array(create_random_circular_points([0,0],2,numPoints))    
-logistic_regression(circle1,circle2)
+logistic_regression(circle1,circle2,True)
