c810f85dc117bf4e3a81ff5a2e694fcac5054581
Leora <leorab@suite-sol.com>
2019-04-30 14:45:55 +0300

    added max_depth and improved printing of tree

--- ex9_decision_trees_gini.py
+++ ex9_decision_trees_gini.py
@@ -9,82 +9,83 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 
-class DataSet():
-    def __init__(self,train):
-        self.train = train
-
 class Node():
-    def __init__(self,split_feature,split_value,left_child,right_child):
+    def __init__(self, split_feature, split_value, left_child, right_child, node_level):
         self.split_feature = split_feature
         self.split_value = split_value
         self.left_child = left_child
         self.right_child = right_child
+        self.depth = node_level 
         
-    def display(self):
-        print("feature", self.split_feature,"value", self.split_value)
-        self.left_child.display()
-        self.right_child.display()
+    def display(self, spacing=" "):    
+        print(spacing + "feature", self.split_feature,"value", self.split_value, "node level", self.depth)    
+        print(spacing + '--> Left:')
+        self.left_child.display(spacing + "  ")
+        print(spacing + '--> Right:')
+        self.right_child.display(spacing + "  ")
         
-    def predict(self,row):
+    def predict(self, row):
         if row[self.split_feature] > self.split_value:
             return self.left_child.predict(row)
         return self.right_child.predict(row)
 
 class Leaf():
-    def __init__(self,classValue):
-        #Either M or B
+    def __init__(self, classValue):
         self.value = classValue
     
-    def display(self):
-        print("class",self.value)
+    def display(self, spacing=" "):
+        print(spacing + "class",self.value)
         
-    def predict(self,row):
+    def predict(self, row):
         return self.value
 
-class BinaryTree(DataSet):
-    def __init__(self):
+class DecisionTreeClassifier():
+    def __init__(self, max_depth):
         self.root = None
+        self.max_depth = max_depth
         
     def fit(self, train):
-        DataSet.__init__(self, train)
-        rootNode = self.build_tree(train)
-        self.set_root(rootNode)
-        #Commenting out because I am not satisfied with my tree display implementation
+        self.train = train
+        # The build_tree method returns a node.  Assign this as the root node.
+        self.set_root(self.build_tree(train))
         #self.print_tree()
     
-    def set_root(self,node):
+    def set_root(self, node):
         if self.root == None:
             self.root = node
 
-    def build_tree(self,rows):
-        if self.calc_gini(rows) ==0:
-            leafValue = rows.iloc[0,1]
-            return Leaf(leafValue)
+    def build_tree(self, rows, node_level=0):
+        node_level +=1
+        
+        if self.calc_gini(rows) == 0 or node_level > self.max_depth :
+            #If gini is 0 we found a class, i.e. all items are in the same class.  
+            #Take the first class value and create a leaf node.
+            return Leaf(rows.iloc[0,1])
         
         best_gain, best_split_feature, best_split_value = self.find_best_split(rows)
         left_rows, right_rows = self.data_split(rows, best_split_feature, best_split_value)                    
-        left_child = self.build_tree(left_rows)
-        right_child = self.build_tree(right_rows)
+        left_child = self.build_tree(left_rows, node_level)
+        right_child = self.build_tree(right_rows, node_level)
         
-        return Node(best_split_feature, best_split_value,left_child,right_child)
+        return Node(best_split_feature, best_split_value, left_child, right_child, node_level)
     
     def print_tree(self):
-        self.root.display()
+        self.root.display(spacing="")
         
     def make_predictions(self, test):
         test_labels = []
+        y_test = test.iloc[:,1].tolist()
   
         for rowNum, row_data in enumerate(test.itertuples(index=False)):
-            row_label = self.root.predict(row_data)
-            test_labels.append(row_label)
+            test_labels.append(self.root.predict(row_data))
         
-        return test_labels
+        self.get_accuracy(test_labels, y_test)
         
-    def get_accuracy(self,predictions,Y_test):
+    def get_accuracy(self, predictions, Y_test):
         correct = np.asarray(Y_test) == np.asarray(predictions)
-        print("Accuracy of predictions", (np.sum(correct)/float(len(Y_test))) * 100.0)
+        print("prediction accuracy for tree with max depth of",self.max_depth, (np.sum(correct)/float(len(Y_test))) * 100.0)
     
-    def class_counts(self,classes):
+    def class_counts(self, classes):
         counts = {}  # a dictionary of label -> count.
         for label in classes:
             if label not in counts:
@@ -92,50 +93,49 @@ class BinaryTree(DataSet):
             counts[label] += 1
         return counts
       
-    def data_split(self,dataset, feature_idx, split_val):
-        leftTree, rightTree = [], []
-        leftTree = dataset[dataset[feature_idx] > split_val]
-        rightTree = dataset[dataset[feature_idx] <= split_val]     
-        return pd.DataFrame(leftTree), pd.DataFrame(rightTree)
+    def data_split(self, dataset, feature_idx, split_val):
+        #left_tree, right_tree = [], []
+        left_tree = dataset[dataset[feature_idx] > split_val]
+        right_tree = dataset[dataset[feature_idx] <= split_val]     
+        return pd.DataFrame(left_tree), pd.DataFrame(right_tree)
      
     # The calc_gini impurity can be computed by summing the probability of an item being chosen
     # times the probability of a mistake in categorizing that item.
-    def calc_gini(self,rows):    
+    def calc_gini(self, rows):    
         counts = self.class_counts(rows.iloc[:,1])
         probabilities =  [x / float(len(rows)) for x in counts.values()]      
         return 1 - sum([p**2 for p in probabilities])
     
     #The uncertainty of the starting node, minus the weighted impurity of two child nodes
-    def info_gain(self,left, right, current_uncertainty):
+    def info_gain(self, left, right, current_uncertainty):
         p = (float(len(left)) / (len(left) + len(right)))
         return current_uncertainty - p * self.calc_gini(left) - (1 - p) * self.calc_gini(right)
     
-    def find_best_split(self,rows):
+    def find_best_split(self, rows):
         best_gain = 0  
         best_split_feature = 0
         best_split_value = 0
         current_uncertainty = self.calc_gini(rows)
         n_features = rows.shape[1] 
         
-        for col in range(2,n_features):
+        for col in range(2, n_features):
             values = rows[col].unique()        
             for val in values:
                 left_rows, right_rows = self.data_split(rows, col, val)
                 gain = self.info_gain(left_rows, right_rows, current_uncertainty)
                 if gain > best_gain:
                     best_gain, best_split_feature, best_split_value = gain, col, val
-        print("best_split_feature",best_split_feature,"best_split_value",best_split_value)
         return best_gain, best_split_feature, best_split_value 
 
 def main():
     wdbc = pd.read_csv('wdbc.data.txt',header=None)
-    train , test = train_test_split(wdbc, test_size=0.2) 
-    y_test = test.iloc[:,1].tolist()
+    train_data, test_data = train_test_split(wdbc, test_size=0.2, random_state=88) 
     
-    tree = BinaryTree()
-    tree.fit(train)
-    predictions = tree.make_predictions(test)
-    tree.get_accuracy(predictions,y_test)
+    #Create classifier objects with varied tree depths so we can determine optimal depth
+    for max_depth in range(1,7):
+        tree = DecisionTreeClassifier(max_depth)
+        tree.fit(train_data)
+        tree.make_predictions(test_data)
 
 if __name__ == '__main__':
     main()
